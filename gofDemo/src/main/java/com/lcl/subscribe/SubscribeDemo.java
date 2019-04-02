8ac378665247d2d54c7b3ce719352e1c3f816bc1
lichenglong1 <lichenglong1@longfor.com>
2019-04-02 16:33:11 +0800

    发布订阅demo

---
+++ gofDemo/src/main/java/com/lcl/subscribe/SubscribeDemo.java
@@ -0,0 +1,14 @@
+package com.lcl.subscribe;
+
+/**
+ * @ClassName: SubscribeDemo
+ * @Description: 发布订阅demo
+ * @Author: lichenglong
+ * @Date: 2019/4/2 16:31
+ */
+public class SubscribeDemo {
+
+
+
+
+}
